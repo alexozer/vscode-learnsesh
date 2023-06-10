d7aec6437d84a6bdb385541c5f1bba613800a2b4
Alex Ozer <alexozer@users.noreply.github.com>
2023-06-10 15:03:33 -0400

    Date format yet again

--- src/extension.ts
+++ src/extension.ts
@@ -22,10 +22,11 @@ async function createSessionFolder(
     const year = String(date.getFullYear());
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
-    const time = getTimeStr(date);
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
 
     const monthDirName = `${year}_${month}`;
-    const minutesDirName = `${year}${month}${day}-${time}-${sessionName}`;
+    const minutesDirName = `${year}${month}${day}_${hours}${minutes}_${sessionName}`;
 
     const minutesDirUri = folder.with({
         path: posix.join(folder.path, monthDirName, minutesDirName),
